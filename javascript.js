5d947c9e36b8b6fe9cc7081a79db17c06ebfcaa3
Sokitaru <154542214+Sokitaru@users.noreply.github.com>
2024-10-22 18:43:02 -0700

    Create a function to generate random numbers to represent rock, paper, or scissors
    Using the math.random function allows it to generate a number and multiply that number times 3 to generate three numbers. This allows it to use if statements to return rock, paper or scissors  matching those three numbers

---
+++ javascript.js
@@ -0,0 +1,35 @@
+
+function toggle() {
+    let q = document.querySelectorAll('#nightify')
+    if(q.length) {
+        q[0].parentNode.removeChild(q[0])
+        return false
+    }
+    var h = document.getElementsByTagName('head')[0],
+        s = document.createElement('style');
+    s.setAttribute('type', 'text/css');
+    s.setAttribute('id', 'nightify');
+    s.appendChild(document.createTextNode('html{-webkit-filter:invert(100%) hue-rotate(180deg) contrast(70%) !important; background: #fff;} .line-content {background-color: #fefefe;}'));
+    h.appendChild(s); 
+    return true
+}
+
+toggle() // woa! everything is dark!
+
+// create function getComputerChoice
+function getComputerChoice(){
+    let random = Math.floor(Math.random() * 3); // put random number in random
+   // IF random number equals 0, return word "rock"
+   if (random === 0) {
+   return console.log("rock");
+   }
+   // IF random number equals 1, return word "paper"
+   else if (random === 1) {
+    return console.log("paper");
+   }
+   // IF neither those numbers, return word "scissors"
+   else {return console.log("scissors")}
+
+}
+getComputerChoice();
+
